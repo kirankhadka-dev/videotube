125df50134ab005af20e3f33bced13ce877f6a59
Kiran Khadka <kiran.khadka.dev@gmail.com>
2024-10-31 06:09:22 +1100

    Configure cloudinary service for file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from "cloudinary";
+
+import fs from " fs";
+
+// Configuration
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return null;
+    }
+
+    // Upload  the file on cloudinary
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // file has been uploaded successfully
+
+    console.log("file has been  uploaded successfully", response.url);
+
+    return response;
+  } catch (error) {
+    // remove file from server :
+    fs.unlinkSync(localFilePath); // remove the  locally saved temp file as the upload failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
