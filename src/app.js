20ad4a6fff97c002896fbf058844d61a33c34b6c
Kiran Khadka <kiran.khadka.dev@gmail.com>
2024-10-31 14:59:30 +1100

    All setup done

--- src/app.js
+++ src/app.js
@@ -21,4 +21,10 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+// Routes import
+import userRouter from "./routes/user.route.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
 export { app };
