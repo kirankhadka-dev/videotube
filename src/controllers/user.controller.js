a6c7e83f76cb9477311a33ecca1ed834e20ef5cb
Kiran Khadka <kiran.khadka.dev@gmail.com>
2024-11-02 13:10:53 +1100

    Add  user management routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -486,7 +486,7 @@ const getWatchHistory = asyncHandler(async (req, res) => {
       new ApiResponse(
         200,
         user[0].watchHistory,
-        "History is fetched sucessfully"
+        "Watch history  is fetched sucessfully"
       )
     );
 });
@@ -502,4 +502,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory,
 };
