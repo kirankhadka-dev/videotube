c950752502f432c3030e2e0cb0c97a24193147fd
Kiran Khadka <kiran.khadka.dev@gmail.com>
2025-02-05 17:35:59 +1100

    Set up Express middleware with CORS and cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,32 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// Handle json data
+app.use(express.json({ limit: "16kb" }));
+
+// Handle url data :
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+// Handle static assets
+app.use(express.static("public"));
+
+// Handle cookie data :
+app.use(cookieParser());
 
 export default app;
