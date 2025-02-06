b3c119a3488c5caae2b346a36a8e246138134778
Kiran Khadka <kiran.khadka.dev@gmail.com>
2025-02-07 09:18:41 +1100

    Implement getUserChannelProfile and getWatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const registerUser = asyncHandler(async (req, res) => {
   // Get user details from frontend
@@ -355,6 +356,161 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
   }
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    if (!username?.trim()) {
+      throw new ApiError(400, "username is missing ");
+    }
+
+    const channel = await User.aggregate([
+      {
+        $match: {
+          username: username?.toLowerCase(),
+        },
+      },
+
+      {
+        // Finding  the subsribers
+        $lookup: {
+          from: "subscriptions", //Model that is saved in the db
+          localField: "_id",
+          foreignField: "channel",
+          as: "subscribers",
+        },
+      },
+
+      {
+        // finding the subcribed numbers
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "subscriber",
+          as: "subscribedTo",
+        },
+      },
+
+      {
+        $addFields: {
+          subscribersCount: {
+            $size: "$subscribers",
+          },
+          channelsSubscribeToCount: {
+            $size: "$subscribeTo",
+          },
+          isSubscribed: {
+            $cond: {
+              if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+              then: true,
+              else: false,
+            },
+          },
+        },
+      },
+
+      {
+        // What to return  upon the query :
+        $project: {
+          fullname: 1,
+          username: 1,
+          subscribersCount: 1,
+          channelsSubscribeToCount: 1,
+          isSubscribed: 1,
+          avatar: 1,
+          coverImage: 1,
+          email: 1,
+        },
+      },
+    ]);
+
+    if (!channel?.length) {
+      throw new ApiError(404, "channel does not exists");
+    }
+
+    console.log("channel info after aggrregate ", channel);
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          channel[0],
+          "user channel profile fetch successfully "
+        )
+      );
+  } catch (error) {
+    throw new ApiError(
+      500,
+      error?.message ||
+        "something went  wrong while  retrieving channel profile"
+    );
+  }
+});
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+  try {
+    const user = await User.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(req.user._id),
+        },
+      },
+
+      {
+        $lookup: {
+          from: "videos",
+          localField: "watchHistory",
+          foreignField: "_id",
+          as: "watchHistory",
+          pipeline: [
+            {
+              $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                  {
+                    $project: {
+                      fullname: 1,
+                      avatar: 1,
+                      username: 1,
+                    },
+                  },
+                ],
+              },
+            },
+            {
+              // Modify data of owner :
+              $addFields: {
+                owner: {
+                  $first: "$owner",
+                },
+              },
+            },
+          ],
+        },
+      },
+    ]);
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          user[0].watchHistory,
+          "watchHistory fetched successfully"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(
+      500,
+      error?.message || "something went wrong while fetching watch history"
+    );
+  }
+});
+
 export {
   registerUser,
   loginUser,
@@ -365,4 +521,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 };
